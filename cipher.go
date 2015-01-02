f49f63040253d5990c5900b0433cec32e0086f32
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2015-01-01 19:50:43 -0800

    Initial commit.

---
+++ cipher.go
@@ -0,0 +1,84 @@
+package skein
+
+import "crypto/cipher"
+
+var _ cipher.Block = &Threefish{}
+
+// Threefish implements the Threefish-512 block encryption primitive.
+type Threefish struct {
+	k [9]uint64
+	t [3]uint64
+	s [19][8]uint64
+}
+
+// NewCipher returns a Threefish object with the given key.
+func NewCipher(key []byte) *Threefish {
+	if len(key) != 64 {
+		panic("invalid key length")
+	}
+	var b Threefish
+	for i := 0; i < 8; i++ {
+		b.k[i] = le64dec(key[i*8:])
+	}
+	b.expand()
+	return &b
+}
+
+// SetTweak sets the cipher tweak to the given 16 bytes.
+func (b *Threefish) SetTweak(t []byte) {
+	if len(t) != 16 {
+		panic("invalid tweak length")
+	}
+	b.t[0] = le64dec(t[0:])
+	b.t[1] = le64dec(t[8:])
+	// Re-expand the key
+	b.expand()
+}
+
+func (b *Threefish) expand() {
+	expand(&b.s, &b.k, &b.t)
+}
+
+func (*Threefish) BlockSize() int { return 512 / 8 }
+
+func (b *Threefish) Encrypt(dst, src []byte) {
+	if len(src) < b.BlockSize() {
+		panic("src too short")
+	}
+	if len(dst) < b.BlockSize() {
+		panic("dst too short")
+	}
+	var p [8]uint64
+	for i := range p {
+		p[i] = le64dec(src[i*8:])
+	}
+	encrypt512(&p, &b.s)
+	for i, x := range p {
+		le64enc(dst[i*8:][:0], x)
+	}
+}
+
+func (b *Threefish) Decrypt(dst, src []byte) {
+	if len(src) < b.BlockSize() {
+		panic("src too short")
+	}
+	if len(dst) < b.BlockSize() {
+		panic("dst too short")
+	}
+	var p [8]uint64
+	for i := range p {
+		p[i] = le64dec(src[i*8:])
+	}
+	decrypt512(&p, &b.s)
+	for i, x := range p {
+		le64enc(dst[i*8:][:0], x)
+	}
+}
+
+func le64dec(b []byte) uint64 {
+	return uint64(b[0])<<0 | uint64(b[1])<<8 | uint64(b[2])<<16 | uint64(b[3])<<24 | uint64(b[4])<<32 | uint64(b[5])<<40 | uint64(b[6])<<48 | uint64(b[7])<<56
+}
+
+func le64enc(b []byte, x uint64) []byte {
+	return append(b, byte(x), byte(x>>8), byte(x>>16), byte(x>>24), byte(x>>32), byte(x>>40), byte(x>>48), byte(x>>56))
+}
---
+++ gen.go
@@ -0,0 +1,162 @@
+// +build ignore
+
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+	"reflect"
+	"text/template"
+)
+
+var perm512 = [4][8]uint8{
+	{0, 1, 2, 3, 4, 5, 6, 7},
+	{2, 1, 4, 7, 6, 5, 0, 3},
+	{4, 1, 6, 3, 0, 5, 2, 7},
+	{6, 1, 0, 7, 2, 5, 4, 3},
+}
+
+// Rotation constants for Threefish-512.
+var rot512 = [8][4]uint8{
+	{46, 36, 19, 37},
+	{33, 27, 14, 42},
+	{17, 49, 36, 39},
+	{44, 9, 54, 56},
+	{39, 30, 34, 24},
+	{13, 50, 10, 17},
+	{25, 29, 39, 43},
+	{8, 35, 56, 22},
+}
+
+func main() {
+	var buf bytes.Buffer
+	err := tmpl.Execute(&buf, nil)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
+	io.Copy(os.Stdout, &buf)
+}
+
+var funcs = template.FuncMap{
+	"count":   count,
+	"round":   round,
+	"reverse": reverse,
+	"p":       pfunc,
+}
+
+func count(n int) []int {
+	s := make([]int, n)
+	for i := range s {
+		s[i] = i
+	}
+	return s
+}
+
+func reverse(iface interface{}) interface{} {
+	v := reflect.ValueOf(iface)
+	tmp := reflect.New(v.Type().Elem()).Elem()
+	for i, j := 0, v.Len()-1; i < j; i, j = i+1, j-1 {
+		x := v.Index(i)
+		y := v.Index(j)
+		tmp.Set(x)
+		x.Set(y)
+		y.Set(tmp)
+	}
+	return iface
+}
+
+func pfunc(i int) string {
+	return fmt.Sprintf("p[%d]", i)
+}
+
+type subround struct {
+	X, Y, R int
+}
+
+// Returns the indexes and rotation constants for the given round.
+func round(i int) []subround {
+	p := &perm512[i%4]
+	r := &rot512[i%8]
+	return []subround{
+		{int(p[0]), int(p[1]), int(r[0])},
+		{int(p[2]), int(p[3]), int(r[1])},
+		{int(p[4]), int(p[5]), int(r[2])},
+		{int(p[6]), int(p[7]), int(r[3])},
+	}
+}
+
+// mix:   x += y; y <<<= r; y ^= x
+// unmix: y ^= x; y >>>= r; x -= y
+
+// Skein-512 has 72 rounds.
+// Each round consists of four parallel MIX operations and a permutation.
+// Subkeys are integrated every four rounds.
+
+var tmpl = template.Must(template.New("skein").Funcs(funcs).Parse(`
+package skein
+
+//import "fmt"
+
+// Encrypt encrypts a block p with the given subkeys.
+func encrypt512(p *[8]uint64, s *[19][8]uint64) {
+	//fmt.Printf("Initial state: %x\n", p)
+	//fmt.Printf("Key schedule: %x\n", s[0])
+	for i := 0; i < 72; i += 8 {
+		{{ range $i := count 8 }}
+			{{ if eq $i 0 4 }}
+				{{ range $j := count 8 }}
+					{{p $j}} += s[ i/4 + {{$i}}/4 ][ {{$j}} ]
+				{{ end }}
+				//fmt.Printf("State after key injection: %x\n", p)
+			{{ end }}
+			{{ range round $i }}
+				{{p .X}} += {{p .Y}}
+				{{p .Y}} = {{p .Y}}<<{{.R}} | {{p .Y}}>>(64-{{.R}})
+				{{p .Y}} ^= {{p .X}}
+			{{ end }}
+			//fmt.Printf("State after round %d: %x\n", i+{{$i}}+1, p)
+		{{ end }}
+	}
+	{{ range $j := count 8 }}
+		{{p $j}} += s[ len(s)-1 ][ {{$j}} ]
+	{{ end }}
+}
+
+// Decrypt decrypts a block p using the given subkeys.
+func decrypt512(p *[8]uint64, s *[19][8]uint64) {
+	{{ range $j := count 8 }}
+		{{p $j}} -= s[ len(s)-1 ][ {{$j}} ]
+	{{ end }}
+	for i := 72-8; i >= 0; i -= 8 {
+		{{ range $i := count 8 | reverse }}
+			{{ range round $i | reverse }}
+				{{p .Y}} ^= {{p .X}}
+				{{p .Y}} = {{p .Y}}<<(64-{{.R}}) | {{p .Y}}>>{{.R}}
+				{{p .X}} -= {{p .Y}}
+			{{ end }}
+			{{ if eq $i 0 4 }}
+				{{ range $j := count 8 }}
+					{{p $j}} -= s[ i/4 + {{$i}}/4 ][ {{$j}} ]
+				{{ end }}
+			{{ end }}
+		{{ end }}
+	}
+}
+
+// Expand expands a key and tweak into subkeys.
+func expand(s *[19][8]uint64, k *[9]uint64, t *[3]uint64) {
+	t[2] = t[0] ^ t[1]
+	k[8] = c240 ^ k[0] ^ k[1] ^ k[2] ^ k[3] ^ k[4] ^ k[5] ^ k[6] ^ k[7]
+	for i := 0; i < len(*s); i++ {
+		{{ range $j := count 8 }}
+			s[i][ {{$j}} ] = k[ (i + {{$j}})%9 ]
+		{{ end }}
+		s[i][5] += t[ (i + 0)%3 ]
+		s[i][6] += t[ (i + 1)%3 ]
+		s[i][7] += uint64(i)
+	}
+}
+`))
